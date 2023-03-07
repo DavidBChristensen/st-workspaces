7900c6d27fd8708ae627547b070ab0f833ef608f
David Christensen <david.brent.christensen@gmail.com>
2023-03-07 18:37:33 -0500

    Adds a little bit of workspace pathing.

--- src/workspaces.rs
+++ src/workspaces.rs
@@ -1,11 +1,41 @@
+use std::path::PathBuf;
+
+use crate::paths::sourcetree_settings_path;
+
+pub struct Workspaces {
+    pub workspaces: Vec<Workspace>,
+}
+
 pub struct Workspace {
     pub name: String,
+    pub repos: Vec<String>,
+}
+
+impl Workspace {
+    pub fn new(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            repos: Default::default(),
+        }
+    }
+}
+
+pub fn workspace_path() -> Option<PathBuf> {
+    let settings_path = sourcetree_settings_path();
+    if settings_path.is_none() {
+        return settings_path;
+    }
+
+    Some(settings_path.unwrap().join("st-workspaces.json"))
 }
 
 #[cfg(test)]
 mod tests {
-    //use super::*;
+    use super::*;
 
     #[test]
-    fn should_get_list_of_workspaces() {}
+    fn should_get_list_of_workspaces() {
+        let workspace_path = workspace_path();
+        assert_ne!(workspace_path, None);
+    }
 }
