19445742690fba39bcc1b44fa3d8180b98cea6c3
David Christensen <david.brent.christensen@gmail.com>
2023-03-07 11:29:33 -0500

    Makes colors a little more readable. Added switching between dark and light modes.

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use egui::{FontFamily, FontId, TextStyle};
+use egui::{Color32, FontFamily, FontId, RichText, TextStyle};
 
 use crate::{paths::sourcetree_settings_path, workspaces::Workspace};
 
@@ -25,17 +25,22 @@ impl SourceTreeWorkspacesApp {
 impl eframe::App for SourceTreeWorkspacesApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+            let dark_mode = ui.visuals().dark_mode;
             ui.horizontal(|ui| {
-                ui.heading("SourceTree Workspaces");
+                ui.heading(contrast_text("SourceTree Workspaces", dark_mode));
                 ui.label(format!("(v{})", self.version));
+                egui::widgets::global_dark_light_mode_buttons(ui);
             });
 
             ui.separator();
 
             if self.workspaces.is_empty() {
                 ui.vertical(|ui| {
-                    ui.label("No Workspaces Exist... Yet...");
-                    ui.label("Save one from SourceTree using Custom Action \"Save Workspace\"");
+                    ui.label(contrast_text("No Workspaces Exist... Yet...", dark_mode));
+                    ui.label(contrast_text(
+                        "Save one from SourceTree using Custom Action \"Save Workspace\"",
+                        dark_mode,
+                    ));
                 });
             } else {
                 ui.vertical(|ui| {
@@ -81,4 +86,12 @@ pub fn configure_text_styles(ctx: &egui::Context) {
     ]
     .into();
     ctx.set_style(style);
+}
+
+pub fn contrast_text(text: &str, dark_mode: bool) -> RichText {
+    if dark_mode {
+        RichText::new(text).color(Color32::from_rgb(200, 200, 200))
+    } else {
+        RichText::new(text).color(Color32::from_rgb(10, 10, 10))
+    }
 }
